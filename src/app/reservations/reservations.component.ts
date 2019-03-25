0793c16f7e836c8d8a218f72d00c6b61d9ea566e
Katrinape <katrina3447e@gmail.com>
2019-03-25 13:21:08 +0100

    Add expanded details to reservations

--- src/app/reservations/reservations.component.ts
+++ src/app/reservations/reservations.component.ts
@@ -2,15 +2,24 @@ import {Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {EventItem} from '../events/event-item';
 import {EventService} from '../events/event.service';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
-  styleUrls: ['./reservations.component.css']
+  styleUrls: ['./reservations.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0', display: 'none'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ReservationsComponent implements OnInit {
   displayedColumns = ['id', 'title', 'start', 'description', 'customer'];
   reservations: EventItem[];
+  expandedElement: EventItem | null;
   dataSource: MatTableDataSource<EventItem>;
   @ViewChild(MatSort) sort: MatSort;
   @ViewChild(MatPaginator) paginator: MatPaginator;
@@ -31,6 +40,16 @@ export class ReservationsComponent implements OnInit {
             default: return item[property];
           }
         };
+        this.dataSource.filterPredicate = (data: EventItem, filter: string) => {
+          const dataStr =
+            data.id + ' ' +
+            data.title + ' ' +
+            data.start + ' ' +
+            data.description + ' ' +
+            data.customerEntity.name + ' ' +
+            data.customerEntity.surname;
+          return dataStr.trim().toLocaleLowerCase().indexOf(filter) != -1;
+        }
       },
       error1 => console.log(error1)
     );
